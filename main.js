80497746686ea8d33694229d3bd600b7b923c480
Rebeka Filipović <rebeka.filipovich@gmail.com>
2023-03-21 17:25:13 +0100

    changes on the sass and main.js

--- main.js
+++ main.js
@@ -30,7 +30,52 @@
 
             task_content_el.append(task_input_el);
 
+            const task_actions_el =$('<div>');
+            task_actions_el.addClass('actions');
+            
+            const task_edit_el = $('<button>');
+            task_edit_el.addClass('edit');
+            task_edit_el.text('Edit');
+
+            const task_delete_el = $('<button>');
+            task_delete_el.addClass('delete');
+            task_delete_el.text('Delete');
+            
+            task_actions_el.append(task_edit_el);
+            task_actions_el.append(task_delete_el);
+
+            task_el.append(task_actions_el);
+
             list_el.append(task_el);
+
+            input.val('');
+
+            task_edit_el.on('click', (e) =>{
+                /*task_input_el.removeAttr('readonly');
+                task_input_el.focus();
+                task_edit_el.removeClass('edit');
+                task_edit_el.addClass('save');
+                task_edit_el.text('Save');*/
+                if(task_edit_el.text().toLowerCase() == "edit"){
+                    task_input_el.removeAttr('readonly');
+                    task_input_el.focus();
+                    task_edit_el.css('background-color','#3DCA3D');
+                    task_edit_el.css('color','#f4decb')
+                    task_edit_el.text('Save');
+                }else{
+                    task_edit_el.css('background-color','#B25068');
+                    task_edit_el.text('Edit');
+                    
+                    task_input_el.attr('readonly','readonly');
+                    alert('The changes have been saved!');                      
+                }
+            });
+
+            task_delete_el.on('click', ()=>{
+                list_el.find(task_el).remove();
+            });
         });
+
+        input.val('');
     });
 })(jQuery);
